2f4fbee9ce2bfe3977e335786410f8cf16f40e8c
dani-rl <danielralo95@gmail.com>
2020-10-22 19:37:27 +0200

    Retocar el codigo para crear una funcion

--- SQL_Connection.py
+++ SQL_Connection.py
@@ -1,16 +1,42 @@
+## -*- coding: utf-8 -*-
+"""
+@author: dani-rl
+"""
 import pandas as pd
 import pyodbc
-connection_string(
-    'DRIVER = MySQL ODBC 8.0 ANSI Driver;'
-    'SERVER = ip_servidor;'
-    'DATABASE = database_name'
-    'UID = user_name;'
-    'PWD = password;'
-    'CHARSET = deafult_charset;'
-)
 
-conn = pyodbc.connect(connection_string)
-cur = conn.cursor()
 
-sql_query = pd.read_sql_query('SELECT * FROM table_name', conn)
-print(sql_query)
+def obtenerTabla(table_name):
+
+    """
+    Esta funcion nos permite crear la conexion con nuestra BBDD
+    y seleccionar una tabla. Esta tabla se devuelve como dataframe.
+
+    Parametros
+    ----------
+    table_name : string
+        Nombre de la tabla en SQL que queremos seleccionar
+
+    Return
+    ------
+    dataframe
+        Dataframe de la tabla completa seleccionada
+
+    """
+
+    connection_string(
+        'DRIVER = MySQL ODBC 8.0 ANSI Driver;'
+        'SERVER = ip_servidor;'
+        'DATABASE = database_name;'
+        'UID = user_name;'
+        'PWD = password;'
+        'CHARSET = deafult_charset;'
+    )
+
+    conn = pyodbc.connect(connection_string)
+    cur = conn.cursor()
+
+    sql_query = pd.read_sql_query('SELECT * FROM ' + table_name, conn)
+    print(sql_query)
+
+    conn.close()
