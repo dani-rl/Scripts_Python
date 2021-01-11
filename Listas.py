b79626c84d2820d96a5be7dd16b0801631fc9029
dani-rl <danielralo95@gmail.com>
2021-01-11 20:16:52 +0100

    Ejercicios de Listas y update de SQL_Connection

---
+++ Listas.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+Autor: dani-rl
+"""
+
+
+"""
+-------------------------- BLOQUE LISTAS --------------------------
+Lista: ordenada, mutable, permite duplicados
+"""
+
+miLista = ["coche","moto","autobus"]
+miLista
--- SQL_Connection.py
+++ SQL_Connection.py
@@ -1,33 +1,37 @@
 # -*- coding: utf-8 -*-
 
+"""
+Autor: dani-rl
+"""
+
 import pandas as pd
 import pyodbc
 
 class funcionesSQL:
-    
+
     """
-    
+
     Conjunto de funciones creadas para mostrar, obtener e insertar
     datos en una BBDD de MySQL
-    
+
     """
 
     def creaConexion(driver, ip_servidor, database_name,
                      user_name, password, charset):
-        
+
         """
-        
+
         Esta funcion nos permite crear la conexion con nuestra BBDD
         y seleccionar una tabla. Esta tabla se devuelve como dataframe.
         El controlador instalado debe ser Connector/ODBC x64. La instalacion
         se puede hacer desde MySQL Installer
-        
+
         Parametros
         ----------
         driver : string
             Driver necesario para conctarse al servidor
         ip_servidor : string
-            Nombre o direccion ip del servidor 
+            Nombre o direccion ip del servidor
         database_name : string
             Nombre de la base de datos
         user_name : string
@@ -36,14 +40,14 @@ class funcionesSQL:
             Contraseña para acceder a la base de datos
         charset : string
             Conjunto de caracteres alfanumericos que utiliza la BBDD
-         
+
         Return
         ------
         conn
             Devuelve la conexion establecida con la BBDD
-            
+
         """
-        
+
         #Crea conexion a BBDD
         connection_string = (
             'DRIVER={'+driver+'};'
@@ -53,71 +57,71 @@ class funcionesSQL:
             'PWD='+password+';'
             'CHARSET='+charset+';'
         )
-        
+
         conn = pyodbc.connect(connection_string)
         #cur = conn.cursor()
-        
+
         return conn
-        
+
     def mostrarTabla(conn, table_name):
-    
+
         """
-        
+
         Esta funcion nos permite crear la conexion con nuestra BBDD
         y seleccionar una tabla.
         El controlador instalado debe ser Connector/ODBC x64. La instalacion
         se puede hacer desde MySQL Installer
-        
+
         Parametros
         ----------
         conn: connection
             Conexion creada con la BBDD de MySQL
         table_name : string
             Nombre de la tabla en SQL que queremos seleccionar
-            
+
         Return
         ------
         No devuelve ningun valor
-        
+
         """
         #Selecciona una tabla de una base de datos de MySQL
         sql_query = pd.read_sql_query('SELECT * FROM ' + table_name, conn)
         print(sql_query)
-    
-    
+
+
     def obtenerTabla(conn, table_name):
-    
+
         """
-        
+
         Esta funcion nos permite crear la conexion con nuestra BBDD
         y seleccionar una tabla. Esta tabla se devuelve como dataframe.
         El controlador instalado debe ser Connector/ODBC x64. La instalacion
         se puede hacer desde MySQL Installer
-        
+
         Parametros
         ----------
         conn: connection
             Conexion creada con la BBDD de MySQL
         table_name : string
             Nombre de la tabla en SQL que queremos seleccionar
-            
+
         Return
         ------
         DataFrame
             Devuelve un dataframe con la tabla seleccionada
-        
+
         """
         #Selecciona una tabla de una base de datos de MySQL
         sql_query = pd.read_sql_query('SELECT * FROM ' + table_name, conn)
         return sql_query
-        
+
     def insertaDatos(conn,df, table_name):
-            
+
         """
-        
+
         Esta funcion permite insertar los datos de un DataFrame en una BBDD
-        de MySQL. 
-        
+        de MySQL.
+
         Parametros
         ----------
         conn: connection
@@ -128,15 +132,15 @@ class funcionesSQL:
             Ruta completa del csv que queremos insertar en la tabla
         file_name: string
             Nombre completo del csv con la extensión .csv
-            
+
         Return
         ------
         No devuleve ningun valor
-        
+
         """
         cur = conn.cursor()
         #Insertar datos de DataFrame en MySQL:
-    
+
         cols = ",".join([str(i) for i in df.columns.tolist()])
 
         # Insert DataFrame recrds one by one.
@@ -146,49 +150,49 @@ class funcionesSQL:
             cur.execute(sql, tuple(row))
 
         cur.commit()
-    
-    
+
+
     #------------------------------ PRUEBAS ------------------------------#
 def main():
-    
-    
-    
+
+
+
     # Datos Conexion #
-    driver = 'MySQL ODBC 8.0 ANSI Driver'
+    driver = 'MySQL ODBC 8.0 Unicode Driver'
     ip_servidor = 'bbdd.braintrust-cs.org'
     database_name = 'btcs'
     user_name = 'btcs'
     password = 'SERVERtrust1.0+-'
     charset = 'latin1'
-    
+
     # Crea conexion #
     conn = funcionesSQL.creaConexion(driver, ip_servidor,
                         database_name, user_name, password, charset)
-    
-    
-    # Insertar datos en tabla #
-    
-    # ----- EN PRRUEBAS ----- #
-    
-    path = 'C:/Users/drlopez/Desktop/Braintrust/Mutua/AUTO/Trabajando/Prueba.csv'
-    
-    df = pd.read_csv(path,sep=';')
-    
 
-    
+    funcionesSQL.mostrarTabla(conn,'V_Parque_DGT')
+    # Insertar datos en tabla #
+
+    # ----- EN PRRUEBAS ----- #
+    """
+    path = 'C:/Users/drlopez/Desktop/Braintrust/Mutua/AUTO/Trabajando/Prueba.csv'
+
+    df = pd.read_csv(path,sep=';')
+
+
+
     try:
-       
+
         funcionesSQL.insertaDatos(conn
              ,df
              ,'Parque_DGT_copy')
-        
+
     except:
         print("Ha ocurrido un error")
     finally:
         conn.close()
-    
-    
-    
+
+    """
+
     # Ejecuta consulta de seleccion #
     # try:
     #     funcionesSQL.mostrarTabla(conn, 'V_Parque_DGT')
